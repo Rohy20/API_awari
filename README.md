6fa832fdf0ce341b3048815e1f1d098ce9074b1c
Rohy Maia <rohymaia@outlook.com>
2023-07-05 21:45:48 -0300

    adicionando projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # API_awari
 Projeto solicitado durante o curso de backend awari, onde eu teria que criar uma API com Express.
+Fiz uma API de cátalogo de filmes, ela retorna um Json com os nomes dos filmes.
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const filmes = require('./src/data/filmes.json')
+
+app.get('/filmes', (req, res) => {
+	return res.json(filmes)
+});
+
+app.listen(8080, () => {
+	console.log('Servidor ta ON....');
+});
